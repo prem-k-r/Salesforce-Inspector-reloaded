3d88eab71a34a85512bf2cdcd592fc53cd53ab0a
Prem Kumar <prem12321kumar@gmail.com>
2026-09-19 20:04:24 +0530

    Revert # 1193

--- addon/popup.js
+++ addon/popup.js
@@ -4059,6 +4059,14 @@ class AllDataSelection extends React.PureComponent {
       return this.getMetadataLink(durableId, "EventObjects");
     } else if (isCustomSetting) {
       return this.getMetadataLink(durableId, "CustomSettings");
+    } else if (sobjectName.endsWith("__c")) {
+      return (
+        "https://"
+        + this.props.sfHost
+        + "/lightning/setup/ObjectManager/"
+        + durableId
+        + "/Details/view"
+      );
     } else {
       return (
         "https://"
@@ -4077,6 +4085,14 @@ class AllDataSelection extends React.PureComponent {
       return this.getMetadataLink(durableId, "CustomMetadata");
     } else if (isCustomSetting) {
       return this.getMetadataLink(durableId, "CustomSettings");
+    } else if (sobjectName.endsWith("__c") || sobjectName.endsWith("__kav")) {
+      return (
+        "https://"
+        + this.props.sfHost
+        + "/lightning/setup/ObjectManager/"
+        + durableId
+        + "/FieldsAndRelationships/view"
+      );
     } else {
       return (
         "https://"
@@ -4117,14 +4133,24 @@ class AllDataSelection extends React.PureComponent {
       + "/ObjectAccess/view"
     );
   }
-  getRecordTypesLink(sfHost, sobjectName) {
-    return (
-      "https://"
-      + sfHost
-      + "/lightning/setup/ObjectManager/"
-      + sobjectName
-      + "/RecordTypes/view"
-    );
+  getRecordTypesLink(sfHost, sobjectName, durableId) {
+    if (sobjectName.endsWith("__c") || sobjectName.endsWith("__kav")) {
+      return (
+        "https://"
+        + sfHost
+        + "/lightning/setup/ObjectManager/"
+        + durableId
+        + "/RecordTypes/view"
+      );
+    } else {
+      return (
+        "https://"
+        + sfHost
+        + "/lightning/setup/ObjectManager/"
+        + sobjectName
+        + "/RecordTypes/view"
+      );
+    }
   }
   getObjectDocLink(sobject, api) {
     if (api === "toolingApi") {
@@ -4279,7 +4305,8 @@ class AllDataSelection extends React.PureComponent {
                           // TODO add check for record type support (such as custom metadata types and custom settings)
                           href: this.getRecordTypesLink(
                             sfHost,
-                            selectedValue.sobject.name
+                            selectedValue.sobject.name,
+                            selectedValue.sobject.durableId
                           ),
                           target: linkTarget,
                           onClick: handleLightningLinkClick,
